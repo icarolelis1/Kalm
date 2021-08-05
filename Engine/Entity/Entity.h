9d03f07e5cd3da391ab426d4b9e2805874516ad0
icarolelis1 <icaroleles1@gmail.com>
2021-08-05 19:03:31 -0300

    >

--- Engine/Entity/Entity.h
+++ Engine/Entity/Entity.h
@@ -89,8 +89,8 @@ namespace Engine {
 		std::shared_ptr<Engine::Component> getComponent(std::string name);
 		std::shared_ptr<Engine::Component> getComponent(Engine::COMPONENT_TYPE componentType);
 
-		Transform tranform;
-
+		Transform transform;
+		//CHANGING GITHUB STUFF
 	protected:
 		bool alive = true;
 		bool displayOnInspector = true;
